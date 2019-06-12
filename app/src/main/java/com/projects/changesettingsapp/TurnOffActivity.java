6f3fb431cb29a3ece3395d73eb7a66b51c51343a
svtkrp <svtkrp@gmail.com>
2019-06-12 03:37:02 +0300

    first commit

---
+++ app/src/main/java/com/projects/changesettingsapp/TurnOffActivity.java
@@ -0,0 +1,74 @@
+package com.projects.changesettingsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class TurnOffActivity extends AppCompatActivity {
+
+    Button mTurnOffButton;
+    TimePicker mTimePicker;
+    Button mCancelButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turn_off);
+
+        mTimePicker = findViewById(R.id.time_picker);
+        mTimePicker.setIs24HourView(true);
+
+        mTurnOffButton = findViewById(R.id.turn_off_button);
+        mTurnOffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+
+                long msTimer = getSecondsAmountBetweenTimes(Integer.parseInt
+                                (new SimpleDateFormat("HH").format(calendar.getTime())),
+                        Integer.parseInt(new SimpleDateFormat("mm").format(calendar.getTime())),
+                        Integer.parseInt(new SimpleDateFormat("ss").format(calendar.getTime())),
+                        mTimePicker.getCurrentHour(), mTimePicker.getCurrentMinute(), 0)
+                        * 1000;
+
+                String currentTime = new SimpleDateFormat("HH:mm:ss").format(calendar.getTime());
+
+                StringBuilder timerTimeBuilder = new StringBuilder();
+                if (mTimePicker.getCurrentHour() < 10) timerTimeBuilder.append("0");
+                timerTimeBuilder.append(mTimePicker.getCurrentHour()).append(":");
+                if (mTimePicker.getCurrentMinute() < 10) timerTimeBuilder.append("0");
+                String timerTime = timerTimeBuilder.append(mTimePicker.getCurrentMinute())
+                        .append(":00").toString();
+
+                startService(TurnOffService.newIntent(TurnOffActivity.this,
+                        msTimer, currentTime, timerTime));
+            }
+        });
+
+        mCancelButton = findViewById(R.id.cancel_button);
+        mCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(TurnOffActivity.this, TurnOffService.class));
+            }
+        });
+    }
+
+    long getSecondsAmountBetweenTimes(int currentHours, int currentMinutes, int currentSeconds,
+                                 int nextHours, int nextMinutes, int nextSeconds) {
+        long secondsInDay = 24*3600;
+        long currentSecondsSum = (currentHours * 60 + currentMinutes) * 60 + currentSeconds;
+        long nextSecondsSum = (nextHours * 60 + nextMinutes) * 60 + nextSeconds;
+        long difference = nextSecondsSum - currentSecondsSum;
+        if (difference < 0) {
+            return difference + secondsInDay;
+        }
+        return difference;
+    }
+}
---
+++ app/src/main/java/com/projects/changesettingsapp/TurnOffService.java
@@ -0,0 +1,124 @@
+package com.projects.changesettingsapp;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TurnOffService extends Service {
+
+    private Timer mTimer;
+    private DisableBluetoothTimerTask mDisableBluetoothTimerTask;
+    private long mDelay;
+
+    private static final String DELAY =
+            "com.projects.change_settings_app.delay";
+    private static final String CURRENT_TIME =
+            "com.projects.change_settings_app.current_time";
+    private static final String TIMER_TIME =
+            "com.projects.change_settings_app.timer_time";
+    private static final String CHANNEL_ID = "turn_off_service_channel_id";
+    private static final int NOTIFICATION_ID = 1;
+
+    public TurnOffService() {
+    }
+
+    public static Intent newIntent (Context packageContext, long msTimer,
+                                    String currentTime, String timerTime) {
+
+        Intent intent = new Intent(packageContext, TurnOffService.class);
+        intent.putExtra(DELAY, msTimer);
+        intent.putExtra(CURRENT_TIME, currentTime);
+        intent.putExtra(TIMER_TIME, timerTime);
+
+        return intent;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mTimer != null) {
+            mTimer.cancel();
+        }
+        mTimer = new Timer();
+        mDisableBluetoothTimerTask = new DisableBluetoothTimerTask();
+        mDelay = intent.getLongExtra(DELAY, 5000);
+
+        createNotificationChannel();
+        startForeground(NOTIFICATION_ID, createNotification(intent.getStringExtra(CURRENT_TIME),
+                intent.getStringExtra(TIMER_TIME)));
+
+        mTimer.schedule(mDisableBluetoothTimerTask, mDelay);
+
+        return START_REDELIVER_INTENT;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mTimer != null) {
+            mTimer.cancel();
+        }
+        mTimer = null;
+        mDisableBluetoothTimerTask = null;
+        super.onDestroy();
+    }
+
+    class DisableBluetoothTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            //Disable bluetooth
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetoothAdapter.isEnabled()) {
+                mBluetoothAdapter.disable();
+            }
+            stopSelf();
+        }
+    }
+
+    private Notification createNotification(String currentTime, String timerTime) {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getResources().getString(R.string.notification_title))
+                .setContentText(String.format
+                        (getResources().getString(R.string.short_notification_text), timerTime))
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(String.format
+                                (getResources().getString(R.string.long_notification_text),
+                                        currentTime, mDelay / 1000, timerTime)))
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setOngoing(true)
+                .setShowWhen(true);
+
+        return builder.build();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    getResources().getString(R.string.notification_channel_name),
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(getResources().getString(R.string.notification_channel_description));
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
